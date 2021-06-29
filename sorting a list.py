8ed541a26ca381ef3d517a66ce077270d594ea2f
ishika-789 <82879259+ishika-789@users.noreply.github.com>
2021-06-29 18:22:43 +0530

    Add files via upload

---
+++ sorting a list.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+list = ['Diva Code' ,'Trojan Hash Defiance' ,'The Bug Stops Here' ,'Creep Access' ,'The Bug Slayers' ,'Gopher Hoppers' ,'Bit Rebels' ,'Smash Hack Gremlins']
+print("Printing all hackathon names")
+list.sort(key=len)
+print(list)
